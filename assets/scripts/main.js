22c563c627a81e7068fff2142fda937634a9b785
Xavian Moody <XavianMoody1025@gmail.com>
2024-02-14 13:09:02 -0500

    Added a hamburger mobile icon animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,53 @@
 "use strict";
 
-console.log("Hello World");
+function toggleMobileNavigation() {
+  const navigation = document.querySelector(".header-navigation");
+  const mobileIcon = document.querySelector(".header-burger-icon");
+  const topLine = document.querySelector(".header-burger-icon__top-line");
+  const middleLine = document.querySelector(".header-burger-icon__middle-line");
+  const bottomLine = document.querySelector(".header-burger-icon__bottom-line");
+  const topLineOpenClass = "header-burger-icon__top-line--active";
+  const middleLineOpenClass = "header-burger-icon__middle-line--active";
+  const bottomLineOpenClass = "header-burger-icon__bottom-line--active";
+  const topLineCloseClass = "header-burger-icon__top-line--nonactive";
+  const middleLineCloseClass = "header-burger-icon__middle-line--nonactive";
+  const bottomLineCloseClass = "header-burger-icon__bottom-line--nonactive";
+  let isOpen = false;
+
+  function toggleAction(e) {
+    e.preventDefault();
+
+    function addOpenClasses() {
+      topLine.classList.add(topLineOpenClass);
+      middleLine.classList.add(middleLineOpenClass);
+      bottomLine.classList.add(bottomLineOpenClass);
+      topLine.classList.remove(topLineCloseClass);
+      middleLine.classList.remove(middleLineCloseClass);
+      bottomLine.classList.remove(bottomLineCloseClass);
+    }
+
+    function addCloseClasses() {
+      topLine.classList.remove(topLineOpenClass);
+      middleLine.classList.remove(middleLineOpenClass);
+      bottomLine.classList.remove(bottomLineOpenClass);
+      topLine.classList.add(topLineCloseClass);
+      middleLine.classList.add(middleLineCloseClass);
+      bottomLine.classList.add(bottomLineCloseClass);
+    }
+
+    if (!isOpen) {
+      isOpen = true;
+      navigation.style.transform = "translateX(0)";
+      addOpenClasses();
+    } else {
+      isOpen = false;
+      navigation.style.transform = "translateX(-100%)";
+      addCloseClasses();
+    }
+  }
+
+  mobileIcon.addEventListener("touchstart", toggleAction);
+  mobileIcon.addEventListener("click", toggleAction);
+}
+
+toggleMobileNavigation();
